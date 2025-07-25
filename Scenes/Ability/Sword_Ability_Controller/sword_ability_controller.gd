a679dec441ed81a6d7898c5fe30ad6f0986a2f83
Clockhandle <155466922+Clockhandle@users.noreply.github.com>
2025-07-25 22:00:43 +0700

    add animations to swords, make enemy spawn automatically

--- Scenes/Ability/Sword_Ability_Controller/sword_ability_controller.gd
+++ Scenes/Ability/Sword_Ability_Controller/sword_ability_controller.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const MAX_RANGE = 150
 @export var sword_ability : PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -8,7 +9,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
@@ -16,8 +17,29 @@ func on_timer_timeout() -> void:
 	var player_node = get_tree().get_first_node_in_group("Player") as Node2D
 	if (player_node == null):
 		return
+		
+	var enemy_nodes = get_tree().get_nodes_in_group("Enemy")
+	enemy_nodes = enemy_nodes.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player_node.global_position) < pow(MAX_RANGE, 2)
+	)
+	
+	if (enemy_nodes.size() == 0):
+		return
+	
+	enemy_nodes.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player_node.global_position)
+		var b_distance = b.global_position.distance_squared_to(player_node.global_position)
+		return a_distance < b_distance
+	)
 	
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player_node.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player_node.global_position
+	sword_instance.global_position = enemy_nodes[0].global_position
+	#4 is the offset. we need this so that enemy and sword position isn't the same so that
+	#we can calculate the angles of the sword that will face the enemy.
+	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4 
+	
+	var sword_direction = enemy_nodes[0].global_position - sword_instance.global_position
+	sword_instance.rotation = sword_direction.angle()
+	
 	
--- Scenes/Basic_Enemy/basic_enemy.gd
+++ Scenes/Basic_Enemy/basic_enemy.gd
@@ -5,10 +5,11 @@ var player_node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player_node = get_tree().get_first_node_in_group("Player") as Node2D
+	$Area2D.area_entered.connect(on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
@@ -18,3 +19,7 @@ func get_direction_to_player() -> Vector2:
 	if (player_node != null):
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
+
+
+func on_area_entered(_area: Area2D):
+	queue_free()
