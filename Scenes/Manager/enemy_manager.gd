affb9d829e41c6cc51861f19b9c65dc0ba7fac85
Clockhandle <155466922+Clockhandle@users.noreply.github.com>
2025-07-29 23:20:39 +0700

    add more tiles for tile maps

--- Scenes/Manager/enemy_manager.gd
+++ Scenes/Manager/enemy_manager.gd
@@ -3,12 +3,21 @@ extends Node
 const SPAWN_RANGE = 380
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+
+@onready var timer = $Timer
+
+var base_wait_time
 
 func _ready() -> void:
-	$Timer.timeout.connect(on_timer_timeout)
+	base_wait_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
 func on_timer_timeout() -> void:
+	timer.start()
+	
 	var player_node = get_tree().get_first_node_in_group("Player") as Node2D
 	if (player_node == null):
 		return
@@ -20,4 +29,10 @@ func on_timer_timeout() -> void:
 	var entities_layer = get_tree().get_first_node_in_group("Entities_Layer")
 	entities_layer.add_child(enemy_node)
 	enemy_node.global_position = spawn_position
-	
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	var time_off = (0.1/12) * arena_difficulty
+	time_off = min (time_off, 0.7)
+	print(time_off)
+	timer.wait_time = base_wait_time - time_off
